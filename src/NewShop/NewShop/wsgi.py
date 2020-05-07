b2eca83c9ba6faae9fddfdc29390c1e4f5acad96
GangSeungRyong <onart@skku.edu>
2020-05-07 09:04:45 +0900

    Django

---
+++ src/NewShop/NewShop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NewShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewShop.settings')
+
+application = get_wsgi_application()
--- src/README.md
+++ src/README.md
@@ -1,1 +1,2 @@
-source
+Django 프로젝트만 만듦("NewShop" 디렉토리)
+$ python -m pip install Django
