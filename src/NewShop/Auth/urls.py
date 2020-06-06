3688c0fbd78e92b30d351268d4f87ec26ddbffdd
GangSeungRyong <onart@skku.edu>
2020-06-06 13:08:48 +0900

    이름/비밀번호 찾기

--- src/NewShop/Auth/urls.py
+++ src/NewShop/Auth/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('signup/verification/',views.verify,name='verification'),
     path('logout',views.sign_out,name='logout'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.pw_reset_by_mail, name='pw_reset'),
     path('find-user/',views.id_finder,name='idfind'),
     path('find-pw/',views.pw_finder,name='pwfind'),
+    path('reset-pw/<int:usr>',views.pw_reset,name='pw_reset2'),
+    path('reset-pw/',views.pw_reset2,name='pw_reset3'),
 ]
